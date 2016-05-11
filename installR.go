f739f10319db5ee715ee1a9a23b53dd4935d8f2d
Chris Freeman <cfreeman.code@gmail.com>
2016-05-11 13:12:05 -0500

    Add better error handling for install process

--- installR.go
+++ installR.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func installR(installerFile, installPath string) string {
-	cmd := exec.Command(installerFile, "/SILENT", fmt.Sprintf(`/DIR=%v`, installPath), "/y")
+	installPath = createInstallDir(installerFile, installPath)
+	cmd := exec.Command(installerFile, "/SILENT", fmt.Sprintf(`/DIR=%v`, installPath))
 	err := cmd.Run()
-	errCheck(err)
+	if err == nil {
+		fmt.Println("Install completed. Cleaning up installer file.\n")
+		err := os.Remove(installerFile)
+		errCheck(err)
+	} else {
+		log.Fatal(err)
+	}
 
+	// Create a forward-slash version of the path to the installer so that we can actually use it inside R
 	installDir := filepath.ToSlash(installPath)
+	return installDir
+}
+
+func createInstallDir(installerFile, installPath string) string {
+	fileName := filepath.Base(installerFile)
+	rVersion := strings.TrimSuffix(fileName, "-win.exe")
+	installDir := convertToWindowsPath(filepath.Join(installPath, rVersion))
 	return installDir
 }
